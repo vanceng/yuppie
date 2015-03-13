124431725619906768fa79f5716fc64b095fbf73
VANCE NG <vancejng@gmail.com>
2015-03-12 17:55:43 -0700

    hello pworld

--- renter.rb
+++ renter.rb
@@ -1,11 +1,32 @@
 # must be baller and either furnished or rent cheaper than 2100
 def rent?(furnished, rent, baller)
-  if baller && furnished || rent < 2100
-    return true
-  else
-    return false
-  end
+  (rent < 2100 || furnished) && baller ? "rent" : "don't rent"
 end
+
+puts rent?(true, 1999, false)
+puts rent?(false, 1999, true)
+puts rent?(false, 2200, false)
+puts rent?(true, 2200, true)
+
+# OLD CODE
+# def rent?(furnished, rent, baller)
+#   if rent > 2100 && furnished && baller
+#     return true
+#   elsif 
+#     rent < 2100 && baller 
+#   return true
+#   else
+#     return false
+#   end
+# end
+
+# def rent?(furnished, rent, baller)
+#   if baller && furnished || rent < 2100
+#     return true
+#   else
+#     return false
+#   end
+# end
 
 ###
 # Add your "test" ("driver") code below in order to "test drive" (run) your method above...
